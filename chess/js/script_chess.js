82213a61adcf406b209c3d6032e1784ed8503629
zhulym <zhulym@mail.ru>
2021-05-05 13:26:21 +0300

    added task validation of form and chess table js

--- chess/js/script_chess.js
+++ chess/js/script_chess.js
@@ -1,20 +1,72 @@
 "use strict"
+// /*================================================= HW DOM 3 ======================================================*/
+// //       3. Создать функцию, генерирующую шахматную доску. При этом можно использовать любые html-теги по своему
+// // желанию. Доска должна быть разлинована соответствующим образом, т.е. чередовать черные и белые ячейки.
+// // Строки должны нумероваться числами от 1 до 8, столбцы – латинскими буквами A, B, C, D, E, F, G, H.
+// //       4. (усложненное задание, делайте по желанию) Заполнить созданную таблицу фигурами, фигуры должны выглядеть
+// // как картинки, либо можно использовать символы (существуют символы шахматных фигур) причем все фигуры
+// // должны стоять на своих местах и быть соответственно черными и белыми.
 
-/*================================================= HW DOM 3 ======================================================*/
-//       3. Создать функцию, генерирующую шахматную доску. При этом можно использовать любые html-теги по своему
-// желанию. Доска должна быть разлинована соответствующим образом, т.е. чередовать черные и белые ячейки.
-// Строки должны нумероваться числами от 1 до 8, столбцы – латинскими буквами A, B, C, D, E, F, G, H.
-//       4. (усложненное задание, делайте по желанию) Заполнить созданную таблицу фигурами, фигуры должны выглядеть
-// как картинки, либо можно использовать символы (существуют символы шахматных фигур) причем все фигуры
-// должны стоять на своих местах и быть соответственно черными и белыми.
+let app = {
+    config: {
+        rows: [8, 7, 6, 5, 4, 3, 2, 1],
+        cols: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    },
+
+    run() {
+        // генерируем доску
+        let board = this.generateBoard();
+        // добавляем доску в body
+        document.body.innerHTML = board;
+        // помещаем на доску фигуры, кроме пешек
+        this.insertFiguresOnDesk();
+        // добавляем пешки
+        this.insertPawns();
+        // добавляем колонку с номерами строк
+        this.insertRowsNumbers();
+        // добавляем строку с названиями колонок
+        this.insertColsChars();
+    },
+
+    generateBoard() {
+        let board = "";
+        let rowStartWithColor = 'white';
+        for (let i = 0; i < this.config.rows.length; i++) {
+            let row = "";
+            if (rowStartWithColor == 'white') {
+                row = this.generateRow(rowStartWithColor, this.config.rows[i]);
+                rowStartWithColor = 'black';
+            } else {
+                row = this.generateRow(rowStartWithColor, this.config.rows[i]);
+                rowStartWithColor = 'white';
+            }
+            board += row;
+        }
+        return `<table>${board}</table>`;
+    },
+
+    generateRow(startWithColor, rowNum) {
+        let currentColorClass = startWithColor;
+        let row = "";
+        for (let i = 0; i < this.config.cols.length; i++) {
+            let field = "";
+            if (currentColorClass === 'white') {
+                field = this.generateField('white', rowNum, this.config.cols[i]);
+                currentColorClass = 'blackField';
+            } else {
+                field = this.generateField('black', rowNum, this.config.cols[i]);
+                currentColorClass = 'white';
+            }
+            row += field;
+        }
+        return `<tr>${row}</tr>`;
+    },
+
+    generateField(color, rowNum, colChar) {
+        return `<td data-rownum="${rowNum}" data-colchar="${colChar}" class="${color}"></td>`;
+    },
 
 
-function getNewMyDay(date) {
-    let days = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
-    return days[date.getDay()];
 }
 
-let date = new Date(2014, 0, 3);
-alert(getNewMyDay(date));
-
-
+app.run();
---
+++ validation_form/js/script.js
@@ -0,0 +1,148 @@
+let form = document.forms[0];
+form.addEventListener('submit', function (event, form) {
+    if (!name.validate() || !phone.validate() || !passwords.validate()) {
+        event.preventDefault();
+    }
+})
+
+let validators = {
+    validateLength(min, max, value) {
+        return value.length >= min && value.length <= max;
+    },
+
+    validateForNumber(value) {
+        return typeof value === 'number' && !isNaN(value);
+    }
+}
+
+
+let name = {
+
+    name: 'Name',
+
+    rules: {
+        min: 1,
+        max: 50
+    },
+
+    getField() {
+        return document.getElementById('name');
+    },
+
+    getFieldValue() {
+        return this.getField().value;
+    },
+
+    validate() {
+        let valid = validators.validateLength(this.rules.min, this.rules.max, this.getFieldValue());
+        if (!valid) {
+            this.showError();
+            return false;
+        }
+        this.setValid();
+        return true
+    },
+
+    showError() {
+        this.getField().classList.add('is-invalid');
+        document.querySelector('.nameError').innerHTML = `Длина поля ${this.name} должна быть больше ${this.rules.min} и меньше ${this.rules.max}`;
+    },
+
+    setValid() {
+        let field = this.getField();
+        field.classList.remove('is-invalid');
+        field.classList.add('is-valid');
+
+    }
+};
+
+let phone = {
+
+    name: 'Phone',
+
+    rules: {
+        type: "number",
+        typeRu: "число",
+        length: 11,
+    },
+
+    getField() {
+        return document.getElementById('phone');
+    },
+
+    getFieldValue() {
+        return this.getField().value;
+    },
+
+    validate() {
+        let validNumber = validators.validateForNumber(parseInt(this.getFieldValue()));
+        let validLength = validators.validateLength(this.rules.length, this.rules.length, this.getFieldValue());
+        if (!validNumber || !validLength) {
+            this.showError();
+            return false;
+        }
+        this.setValid();
+        return true
+    },
+
+    showError() {
+        this.getField().classList.add('is-invalid');
+        document.querySelector('.phoneError').innerHTML = `Длина поля ${this.name} должна быть больше ${this.rules.length} и быть типом ${this.rules.typeRu}`;
+    },
+
+    setValid() {
+        let field = this.getField();
+        field.classList.remove('is-invalid');
+        field.classList.add('is-valid');
+
+    }
+};
+
+let passwords = {
+    namePass: "Пароль",
+    namePassRepeat: "Повтор пароля",
+
+    getFieldPass() {
+        return document.getElementById('password');
+    },
+
+    getFieldPassRepeat() {
+        return document.getElementById('pass-repeat');
+    },
+
+    validate() {
+        let pass = this.getFieldPass().value;
+        let passRepeat = this.getFieldPassRepeat().value;
+        if (pass !== passRepeat || pass === '') {
+            this.showError();
+            return false;
+        }
+        this.setValid();
+        return true
+    },
+
+    showError() {
+        this.getFieldPass().classList.add('is-invalid');
+        this.getFieldPassRepeat().classList.add('is-invalid');
+
+        document.querySelector('.passwordError').innerHTML = 'Пароли должны совпадать, поле ПАРОЛЬ не может быть пустым';
+    },
+
+    setValid() {
+        let fieldPass = this.getFieldPass();
+        fieldPass.remove('is-invalid');
+        fieldPass.add('is-valid');
+
+        let fieldPassRepeat = this.getFieldPassRepeat();
+        fieldPassRepeat.classList.remove('is-invalid');
+        fieldPassRepeat.classList.add('is-valid');
+    }
+
+}
+
+
+
+
+
+
+
