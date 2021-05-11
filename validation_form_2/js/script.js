0b36105cdd34891ce6142b219337b97d34a6882e
zhulym <zhulym@mail.ru>
2021-05-11 15:32:22 +0300

    added js validation form 2

--- validation_form_2/js/script.js
+++ validation_form_2/js/script.js
@@ -8,10 +8,52 @@ document.addEventListener('DomContentLoader', function () {
         e.preventDefault();
 
         let error = formValidate(form);
+
+        if (error === 0) {
+
+        } else {
+            alert('Заполните обязательные поля!')
+        }
     }
 
     function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelector('._req');
 
+        formReq.forEach((item) => {
+            let input = item;
+            formRemoveError(input);
+            if (input.classList.contains('_email')) {
+                if (emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            } else if (input.getAttribute('type') === 'checkbox' && input.checked === false) {
+                formAddError(input);
+                error++;
+            } else {
+                if (input.value === '') {
+                    formAddError(input);
+                    error++;
+                }
+            }
+        })
+        return error;
+    }
+
+
+    function formAddError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+
+    function formRemoveError(input) {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error');
+    }
+
+    function emailTest(input) {
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
     }
 
 
