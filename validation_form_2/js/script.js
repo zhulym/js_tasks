a1d8916061a38927221eb323bcad100f8c21df83
zhulym <zhulym@mail.ru>
2021-05-12 11:27:18 +0300

    added sending js val form 2

--- validation_form_2/js/script.js
+++ validation_form_2/js/script.js
@@ -10,7 +10,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
         let error = formValidate(form);
 
+        let fromData = new FormData(form);
+        fromData.append('image', formImage.files[0]);
+
         if (error === 0) {
+            form.classList.add('_sending');
+            let response = await fetch('sendmail.php', {
+                method: 'POST',
+                body: formData
+            });
+
+            if (response.ok) {
+                let result = await response.json();
+                alert(result.message);
+                formPreview.innerHTML = '';
+                form.reset();
+            } else {
+                alert('Ошибка')
+            }
 
         } else {
             alert('Заполните обязательные поля!')
@@ -23,34 +40,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
         formReq.forEach((item) => {
             let input = item;
-            formRemoveError(input);
+            input.classList.remove('_error');
+
             if (input.classList.contains('_email')) {
                 if (emailTest(input)) {
-                    formAddError(input);
+                    input.classList.add('_error');
                     error++;
                 }
             } else if (input.getAttribute('type') === 'checkbox' && input.checked === false) {
-                formAddError(input);
+                input.classList.add('_error');
                 error++;
             } else {
                 if (input.value === '') {
-                    formAddError(input);
+                    input.classList.add('_error');
                     error++;
                 }
             }
         })
         return error;
-    }
-
-
-    function formAddError(input) {
-        input.parentElement.classList.add('_error');
-        input.classList.add('_error');
-    }
-
-    function formRemoveError(input) {
-        input.parentElement.classList.remove('_error');
-        input.classList.remove('_error');
     }
 
     function emailTest(input) {
@@ -85,9 +92,7 @@ document.addEventListener('DOMContentLoaded', function () {
         reader.onerror = function (event) {
             alert('Ошибка');
         };
-
         reader.readAsDataURL(file);
-
 
     }
 })
