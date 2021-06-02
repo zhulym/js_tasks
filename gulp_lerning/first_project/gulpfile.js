9ae609e516c8983d6fd917ae644bc9e1fa72cfe1
zhulym <zhulym@mail.ru>
2021-06-02 14:02:04 +0300

    lern pug

--- gulp_lerning/first_project/gulpfile.js
+++ gulp_lerning/first_project/gulpfile.js
@@ -1,9 +1,13 @@
 const gulp = require('gulp');
+const gulpPug = require('gulp-pug'); //обработчик для пагов
 
 
 function pug2html(cb) {
     return gulp.src('dev/pug/pages/*.pug') // путь к файлам, которые будем обрабатывать
-        .pipe(dest('output/'));
+        .pipe(gulpPug(                      //  прикручиваем обработчик для пагов
+            { pretty: true }                 // объект для настроек, здесь тру html будет без минификации, остальные настройки в доке
+        ))
+        .pipe(gulp.dest('dist'));   // указываем куда будут помещаться обработанные файлы (папка dist)
 }
 
-exports.default = gulp.series();
+exports.default = gulp.series(pug2html);  // передаём ф-цию 
