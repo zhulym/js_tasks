814a83eb79968fcf2daec39f0b840ba64f7cb7d9
zhulym <zhulym@mail.ru>
2021-06-04 09:52:43 +0300

    added fonts + task +mixin for fonts

--- gulp_lerning/first_project/gulpfile.js
+++ gulp_lerning/first_project/gulpfile.js
@@ -7,11 +7,23 @@ const gulpBabel = require('gulp-babel');
 const gulpUglify = require('gulp-uglify');
 const gulpAutoprefixer = require('gulp-autoprefixer'); //расставит префиксы для стилей по разные браузеры (какие конкретно тоже настраиваем в package.json)
 const gulpCleanCss = require('gulp-clean-css');
+const gulImageMin = require('gulp-imagemin');
+
+const svgSprite = require('gulp-svg-sprite');
+const svgmin = require('gulp-svgmin');
+const cheerio = require('gulp-cheerio');
+const replace = require('gulp-replace');
+
 const browserSync = require('browser-sync').create();
 
 
 function clean() {
     return del('dist');
+}
+
+function fonts() {
+    return gulp.src('dev/static/fonts/**/*.*')
+        .pipe(gulp.dest('dist/static/fonts'));
 }
 
 function pug2html() {
@@ -45,6 +57,51 @@ function script() {
         .pipe(gulp.dest('dist/static/js'));
 }
 
+function imageMin() {
+    return gulp.src([
+        'dev/static/images/**/*.{jpg, gif, png, svg}',
+        '!dev/static/images/sprite/*'
+    ])
+        .pipe(gulImageMin([
+            gulImageMin.gifsicle({ interlaced: true }),
+            gulImageMin.mozjpeg({ quality: 75, progressive: true }),
+            gulImageMin.optipng({ optimizationLevel: 5 }),
+            gulImageMin.svgo({
+                plugins: [
+                    { removeViewBox: true },
+                    { cleanupIDs: false }
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('dist/static/images'));
+}
+
+function svgSpriteBuild() {
+    return gulp.src('dev/static/images/sprite/*.svg')
+        .pipe(svgmin({
+            js2svg: {
+                pretty: true
+            }
+        }))
+        .pipe(cheerio({
+            run: function ($) {
+                $('[fill]').removeAttr('fill');
+                $('[stroke]').removeAttr('stroke');
+                $('[style]').removeAttr('style');
+            },
+            parserOptions: { xmlMode: true }
+        }))
+        .pipe(replace('&gt;', '>'))
+        .pipe(svgSprite({
+            mode: {
+                symbol: {
+                    sprite: "sprite.svg"
+                }
+            }
+        }))
+        .pipe(gulp.dest('dist/static/images/sprite'));
+}
+
 function watch() {
     browserSync.init({
         server: {
@@ -54,9 +111,11 @@ function watch() {
     gulp.watch("dev/pug/**/*.pug", pug2html);               //следим за обн. пагов
     gulp.watch("dev/static/styles/**/*.scss", scss2css);    //следим за обн. сцсс
     gulp.watch("dev/static/js/main.js", script);            // сдедим за обн. js
+    gulp.watch("[dev/static/images/**/*.{jpg, gif, png, svg}, !dev/static/images/sprite/*]", imageMin);            // сдедим за обн. images
+    gulp.watch("dev/static/images/sprite/*", svgSpriteBuild);            // сдедим за обн. svg
 
     gulp.watch("dist/*.html").on('change', browserSync.reload);
 }
 
 
-exports.default = gulp.series(clean, pug2html, scss2css, script, watch);
+exports.default = gulp.series(clean, fonts, pug2html, scss2css, imageMin, svgSpriteBuild, script, watch);
