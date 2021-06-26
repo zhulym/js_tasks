1de783eb6707b90373ba240906555e47df9af6d2
zhulym <zhulym@mail.ru>
2021-06-26 22:31:27 +0300

    feat : add new task

--- JS_TASKS_MIX/_HW_js_tasks/js/script.js
+++ JS_TASKS_MIX/_HW_js_tasks/js/script.js
@@ -542,3 +542,36 @@
 // }
 
 // console.log(capital(mixed_capitals[0]));
+
+
+
+// // 37. Return an array containing the numbers from 1 to N, where N is the parametered value. N will never be less 
+// //     than 1 (in C#, N might be less then 1).
+// //     Replace certain values however if any of the following conditions are met:
+// //     If the value is a multiple of 3: use the value "Fizz" instead
+// //     If the value is a multiple of 5: use the value "Buzz" instead
+// //     If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+
+// const fizzbuzz = n => {
+//     const newArr = [];
+//     let i = 1;
+//     while (i <= n) {
+//         switch (true) {
+//             case (i % 3 === 0 && i % 5 === 0):
+//                 newArr.push('FizzBuzz');
+//                 break;
+//             case (i % 5 === 0):
+//                 newArr.push('Buzz');
+//                 break;
+//             case (i % 3 === 0):
+//                 newArr.push('Fizz');
+//                 break;
+//             default:
+//                 newArr.push(i);
+//         }
+//         i++;
+//     }
+//     return newArr;
+// }
+
+// console.log(fizzbuzz(15))
