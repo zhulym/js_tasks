89e4e2fca6db2e4ec8fa5596ab9a443c2f5b12a7
zhulym <zhulym@mail.ru>
2021-10-24 22:01:15 +0300

    feat: start quiz js

---
+++ _quiz_JS/js/script.js
@@ -0,0 +1,7 @@
+////////////////////////////////
+const x = function () { };
+const y = function (a, b, c) { };
+console.log(x.length); //0
+console.log(y.length); //3
+////////////////////////////////
+
